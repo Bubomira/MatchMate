5efddda4d604e4d4148a3ab4d3e8fdd8ede20098
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-02-26 22:22:41 +0200

    Add project files.

---
+++ MatchMate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MatchMate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
