3e268ea6187f74f04609d610d5025b63b3cc4b70
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-02-27 23:57:04 +0200

    new entitites- offer and friendship

---
+++ MatchMateInfrastructure/Models/Offer.cs
@@ -0,0 +1,55 @@
+﻿using MatchMateInfrastructure.Enums;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static MatchMateInfrastructure.DataConstants.OfferConstants;
+
+namespace MatchMateInfrastructure.Models
+{
+    [Comment("Offer for a meetup, which will be send by one user to another")]
+    public class Offer
+    {
+        [Key]
+        [Required]
+        [Comment("The unique identifyer for an offer")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(MaxTitleLength)]
+        [Comment("The title of the offer")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("The status of the offer, can be rejected, pending, cancelled, accepted")]
+        public OfferStatus Status { get; set; } = OfferStatus.Pending;
+
+        [Required]
+        [MaxLength(MaxDescriptionLength)]
+        [Comment("A brief desription of the activity")]
+        public string Description { get; set; } = string.Empty;
+
+        [Column(TypeName ="decimal(9,6)")]
+        [Comment("Latitude of the offer's location")]
+        public decimal Latitude { get; set; }
+
+        [Column(TypeName = "decimal(9,6)")]
+        [Comment("Longitude of the offer's location")]
+        public decimal Longitude { get; set; }
+
+        [Required]
+        [Comment("The id of the user who made the offer")]
+        public string SuggestingUserId { get; set; } = null!;
+
+        [ForeignKey(nameof(SuggestingUserId))]
+        public ApplicationUser SuggestingUser { get; set; } = null!;
+
+        [Required]
+        [Comment("The id of the user who is offered the offer")]
+        public string ReceivingUserId { get; set; } = null!;
+
+        [ForeignKey(nameof(ReceivingUserId))]
+        public ApplicationUser ReceivingUser { get; set; } = null!;
+
+    }
+}
