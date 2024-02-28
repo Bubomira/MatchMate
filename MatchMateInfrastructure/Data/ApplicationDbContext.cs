225f36595385fe6a5ded5ba1b3415b87a6edb255
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-02-28 23:24:16 +0200

    migrated the database

--- MatchMateInfrastructure/Data/ApplicationDbContext.cs
+++ MatchMateInfrastructure/Data/ApplicationDbContext.cs
@@ -17,5 +17,31 @@ namespace MatchMate.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Friendship> Friendships { get; set; }
         public DbSet<Offer> Offers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserInterest>()
+                .HasKey(ui => new { ui.InterestId, ui.UserId });
+
+            builder.Entity<Friendship>()
+                .HasOne(f => f.Sender)
+                .WithMany(s => s.SendFriendships)
+                .HasForeignKey(f => f.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(m=>m.Sender)
+                .WithMany(s=>s.SendedMessages)
+                .HasForeignKey(m=>m.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Offer>()
+                .HasOne(o=>o.SuggestingUser)
+                .WithMany(su=>su.SuggestedOffers)
+                .HasForeignKey(o=>o.SuggestingUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ MatchMateInfrastructure/Data/Migrations/20240228212242_init-commit.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchMate.Data.Migrations
+{
+    public partial class initcommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterTable(
+                name: "AspNetUsers",
+                comment: "The application user");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true,
+                comment: "Birthday of a user who has to be over 16 years old");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.CreateTable(
+                name: "Friendships",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "The unique identifyer of a friendship")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<int>(type: "int", nullable: false, comment: "The status of a friendship request, can be rejected, pending, active"),
+                    SenderId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "The id of the person who sends a friendship request"),
+                    ReceiverId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "The person who receives a friendship request")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friendships", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Friendships_AspNetUsers_ReceiverId",
+                        column: x => x.ReceiverId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Friendships_AspNetUsers_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                },
+                comment: "A friendship between two users");
+
+            migrationBuilder.CreateTable(
+                name: "Interests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "The unique identifyer for an interest")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, comment: "A name for the interest, will hold the meaning")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "The unique identifyer for a message")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false, comment: "The core of the message"),
+                    ReceiverId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "The id of the person who received the message"),
+                    SenderId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "The id of the person who sended the message")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_AspNetUsers_ReceiverId",
+                        column: x => x.ReceiverId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_AspNetUsers_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                },
+                comment: "Message send by user via the chat");
+
+            migrationBuilder.CreateTable(
+                name: "Offers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "The unique identifyer for an offer")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, comment: "The title of the offer"),
+                    Status = table.Column<int>(type: "int", nullable: false, comment: "The status of the offer, can be rejected, pending, cancelled, accepted"),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false, comment: "A brief desription of the activity"),
+                    Latitude = table.Column<decimal>(type: "decimal(9,6)", nullable: false, comment: "Latitude of the offer's location"),
+                    Longitude = table.Column<decimal>(type: "decimal(9,6)", nullable: false, comment: "Longitude of the offer's location"),
+                    SuggestingUserId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "The id of the user who made the offer"),
+                    ReceivingUserId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "The id of the user who is offered the offer")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Offers_AspNetUsers_ReceivingUserId",
+                        column: x => x.ReceivingUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Offers_AspNetUsers_SuggestingUserId",
+                        column: x => x.SuggestingUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                },
+                comment: "Offer for a meetup, which will be send by one user to another");
+
+            migrationBuilder.CreateTable(
+                name: "UsersInterests",
+                columns: table => new
+                {
+                    InterestId = table.Column<int>(type: "int", nullable: false, comment: "Interest Id, part of the composite key"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "User Id, part of the composite key")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersInterests", x => new { x.InterestId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UsersInterests_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsersInterests_Interests_InterestId",
+                        column: x => x.InterestId,
+                        principalTable: "Interests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friendships_ReceiverId",
+                table: "Friendships",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friendships_SenderId",
+                table: "Friendships",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offers_ReceivingUserId",
+                table: "Offers",
+                column: "ReceivingUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offers_SuggestingUserId",
+                table: "Offers",
+                column: "SuggestingUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersInterests_UserId",
+                table: "UsersInterests",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Friendships");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Offers");
+
+            migrationBuilder.DropTable(
+                name: "UsersInterests");
+
+            migrationBuilder.DropTable(
+                name: "Interests");
+
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterTable(
+                name: "AspNetUsers",
+                oldComment: "The application user");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
