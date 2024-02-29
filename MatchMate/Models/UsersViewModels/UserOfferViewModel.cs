2d1484c03152270ccdedd960a6e33fa1cf9bd71c
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-02-29 23:23:11 +0200

    user model for offer displaying

---
+++ MatchMate/Models/UsersViewModels/UserOfferViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchMate.Models.UsersViewModels
+{
+    public class UserOfferViewModel
+    {
+        public string UserId { get; set; }
+
+        public string Username { get; set; }
+    }
+}
