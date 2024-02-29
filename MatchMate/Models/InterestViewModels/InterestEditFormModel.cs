df2cd01f775ed6e390fc5f648f37cca4db0dabb8
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-02-29 23:39:12 +0200

    interest view models

---
+++ MatchMate/Models/InterestViewModels/InterestEditFormModel.cs
@@ -0,0 +1,11 @@
+﻿using MatchMate.Models.InterestViewModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MatchMate.Models.InterestViewModels
+{
+    public class InterestEditFormModel:InterestPostFormModel
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
---
+++ MatchMate/Models/InterestViewModels/InterestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchMate.Models.InterestViewModels
+{
+    public class InterestViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
