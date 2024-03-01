15da1e78e6461a1e91c184cfb4a6168226035ba5
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-01 22:51:46 +0200

    added mongo model and mongo db constants

---
+++ MatchMateInfrastructure/Mongo/ProfilePicture.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MatchMateInfrastructure.MongoModels
+{
+    public class ProfilePicture
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        public string UserId { get; set; } = string.Empty;
+
+        [BsonRepresentation(BsonType.Binary)]
+        public string Picture { get; set; } = string.Empty;
+    }
+}
