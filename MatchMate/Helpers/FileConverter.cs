26ba8327d9e0329067bbc235e26b1ad34db0319c
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-01 23:59:29 +0200

    convert file to string method

---
+++ MatchMate/Helpers/FileConverter.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MatchMate.Helpers
+{
+    public static class FileConverter
+    {
+        public static string ConvertFormFileToString(IFormFile file)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            file.OpenReadStream().CopyTo(memoryStream);
+
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+    }
+}
