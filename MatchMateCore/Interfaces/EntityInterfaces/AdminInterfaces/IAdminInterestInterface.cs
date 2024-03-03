462ba9396fdabd41c270c5f54f10d803133d87f9
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-03 11:40:20 +0200

    interest user and admin interfaces

---
+++ MatchMateCore/Interfaces/EntityInterfaces/AdminInterfaces/IAdminInterestInterface.cs
@@ -0,0 +1,11 @@
+﻿using MatchMateCore.Dtos.InterestViewModels;
+
+namespace MatchMateCore.Interfaces.EntityInterfaces.AdminInterfaces
+{
+    internal interface IAdminInterestInterface
+    {
+        public Task AddNewInterest(InterestPostFormModel interestPostModel);
+        public Task EditInterest(InterestEditFormModel interestPostModel);
+        public Task DeleteInterest(int interestId);
+    }
+}
---
+++ MatchMateCore/Interfaces/EntityInterfaces/UserInterfaces/IInterestInterface.cs
@@ -0,0 +1,13 @@
+﻿using MatchMateCore.Dtos.InterestViewModels;
+
+namespace MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces
+{
+    internal interface IInterestInterface
+    {
+        public Task<List<InterestModel>> GetAllInterestsAsync();
+        public Task AddInterestToUserCollection(List<InterestModel> interests, string userId);
+        public Task RemoveInterestFromUserCollection(int interestId,string userId);
+        public Task<bool> CheckIfInterestExists(int interestId);
+
+    }
+}
