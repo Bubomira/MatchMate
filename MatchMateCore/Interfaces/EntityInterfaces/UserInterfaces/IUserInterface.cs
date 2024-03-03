9b96a8b194b6280b722d5f2b68ce3dde1e21e270
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-03 21:35:29 +0200

    interface and service for getting users with the same interests

---
+++ MatchMateCore/Interfaces/EntityInterfaces/UserInterfaces/IUserInterface.cs
@@ -0,0 +1,11 @@
+﻿
+
+using MatchMateCore.Dtos.UsersViewModels;
+
+namespace MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces
+{
+    public interface IUserInterface
+    {
+        public Task<List<UserCardModel>> GetUsersWithTheSameInterests(string userId,int pageCount);
+    }
+}
---
+++ MatchMateCore/Services/EntityServices/UserServices/UserService.cs
@@ -0,0 +1,36 @@
+﻿using MatchMate.Data;
+using MatchMateCore.Dtos.UsersViewModels;
+using MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchMateCore.Services.EntityServices.UserServices
+{
+    public class UserService : IUserInterface
+    {
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public async Task<List<UserCardModel>> GetUsersWithTheSameInterests(string userId, int pageCount)
+        {
+            var userInterestIds = await _context.UsersInterests.Where(ui => ui.UserId == userId)
+                .Select(ui => ui.InterestId)
+                .ToListAsync();
+
+            return await _context.UsersInterests.Where(ui => userInterestIds.Contains(ui.InterestId))
+                   .Select(ui => new UserCardModel()
+                   {
+                       UserId = ui.UserId,
+                       Bio = ui.User.Bio,
+                       Username = ui.User.UserName,
+                       Birthday = ui.User.Birthday,
+                       Interests = ui.User.UsersInterests.Select(uui => uui.Interest.Name).ToList()
+                   })
+                   .Skip(3 * pageCount)
+                   .Take(3)
+                   .ToListAsync();
+
+        }
+    }
+}
