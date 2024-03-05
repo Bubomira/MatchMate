7ff9886983c2454063aba885f912399ae26ab481
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-05 21:42:37 +0200

    implemented unit of work pattern

---
+++ MatchMateInfrastructure/UnitOfWork/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchMateInfrastructure.UnitOfWork
+{
+    public interface IRepository
+    {
+        public IQueryable<T> All<T>() where T : class;
+        public IQueryable<T> AllReadOnly<T>() where T : class;
+        public Task Add<T>(T entity) where T : class;
+        public Task Remove<T>(T entity) where T : class;
+        public Task<int> SaveChangesAsync();
+    }
+}
---
+++ MatchMateInfrastructure/UnitOfWork/Repository.cs
@@ -0,0 +1,35 @@
+﻿using MatchMateInfrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MatchMateInfrastructure.UnitOfWork
+{
+    public class Repository : IRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public Repository(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public async Task Add<T>(T entity) where T : class
+        {
+           await DbSet<T>().AddAsync(entity);
+        }
+
+        public IQueryable<T> All<T>() where T : class => DbSet<T>();
+
+        public IQueryable<T> AllReadOnly<T>() where T : class =>
+            DbSet<T>().AsNoTracking();
+     
+
+        public async Task Remove<T>(T entity) where T : class
+        {
+            DbSet<T>().Remove(entity);
+        }
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
+       
+
+        private DbSet<T> DbSet<T>() where T : class => _context.Set<T>();
+    }
+}
