9b3ea485c6821dd0cdf346655efc846ebd238cf4
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-05 22:09:58 +0200

    Added unit of work to IoC

--- MatchMateInfrastructure/UnitOfWork/IRepository.cs
+++ MatchMateInfrastructure/UnitOfWork/IRepository.cs
@@ -4,7 +4,7 @@
     {
         public IQueryable<T> All<T>() where T : class;
         public IQueryable<T> AllReadOnly<T>() where T : class;
-        public Task Add<T>(T entity) where T : class;
+        public Task AddAsync<T>(T entity) where T : class;
         public Task Remove<T>(T entity) where T : class;
         public Task<int> SaveChangesAsync();
     }
