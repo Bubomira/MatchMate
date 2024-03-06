abb22a15cce1e6c4a5ae182ea360a4fb5c43c63d
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-06 21:29:06 +0200

    edit to checker method

--- MatchMateCore/Interfaces/EntityInterfaces/UserInterfaces/IInterestInterface.cs
+++ MatchMateCore/Interfaces/EntityInterfaces/UserInterfaces/IInterestInterface.cs
@@ -7,9 +7,10 @@ namespace MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces
         public Task AddInterestToUserCollectionAsync(int interestId, string userId);
         public Task RemoveInterestFromUserCollectionAsync(int interestId, string userId);
         public Task<bool> CheckIfInterestIsAttachedToUser(int interestId, string userId);
-        public Task<bool> CheckIfUserHasAtLeastThreeInterests(string userId);
+        public Task<bool> CheckIfUserHasAtLeastXInterests(string userId,int comparison);
         public Task<bool> CheckIfInterestExists(int interestId);
         public Task<List<InterestModel>> GetAllInterestsForCurrentUserAsync(string userId);
+
 
     }
 }
