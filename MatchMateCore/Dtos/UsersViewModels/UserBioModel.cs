cebe69208c1bd10f00ff21c33666974bc6ec1b3c
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-06 22:30:08 +0200

    new user bio model

---
+++ MatchMateCore/Dtos/UsersViewModels/UserBioModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static MatchMateInfrastructure.DataConstants.ApplicationUserDataConstants;
+using static MatchMateInfrastructure.ErrorMessages;
+
+namespace MatchMateCore.Dtos.UsersViewModels
+{
+    public class UserBioModel
+    {
+
+        [Required(ErrorMessage =RequiredErrorMessage)]
+        [StringLength(MaxBioLength,MinimumLength =MinBioLength,
+            ErrorMessage =StringLengthErrorMessage)]
+        public string Bio { get; set; }
+    }
+}
