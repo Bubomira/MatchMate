83712038ba6d527bbcbe5d4a3d06a7fe754b06a8
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-07 23:43:26 +0200

    offer interface and service(no implementation) registered into IoC

---
+++ MatchMateCore/Interfaces/EntityInterfaces/UserInterfaces/IOfferInterface.cs
@@ -0,0 +1,16 @@
+﻿using MatchMateCore.Dtos.OfferViewModels;
+
+namespace MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces
+{
+    public interface IOfferInterface
+    {
+        public Task<List<OfferPreviewModel>> GetAllPendingOffers(string userId);
+        public Task<List<OfferPreviewModel>> GetAllAcceptedOffers(string userId);
+        public Task<List<OfferPreviewModel>> GetAllCancelledOffers(string userId);
+        public Task<List<OfferPreviewModel>> GetAllReceivedAndAcceptedOffers(string userId);
+        public Task AddOffer(string senderId);
+        public Task EditOffer(OfferEditFormModel offerEditFormModel);
+        public Task DeleteOffer(int offerId);
+        public Task RejectOffer(int  offerId);
+    }
+}
---
+++ MatchMateCore/Services/EntityServices/UserServices/OfferService.cs
@@ -0,0 +1,54 @@
+﻿using MatchMateCore.Dtos.OfferViewModels;
+using MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces;
+using MatchMateInfrastructure.UnitOfWork;
+
+namespace MatchMateCore.Services.EntityServices.UserServices
+{
+    public class OfferService : IOfferInterface
+    {
+        private readonly IRepository _repository;
+        public OfferService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<OfferPreviewModel>> GetAllCancelledOffers(string userId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task AddOffer(string senderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteOffer(int offerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditOffer(OfferEditFormModel offerEditFormModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OfferPreviewModel>> GetAllAcceptedOffers(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OfferPreviewModel>> GetAllPendingOffers(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OfferPreviewModel>> GetAllReceivedAndAcceptedOffers(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RejectOffer(int offerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
