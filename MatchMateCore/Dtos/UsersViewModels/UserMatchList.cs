78375a5dcd4e461511584c5287cccdf75c731dfc
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-09 17:02:10 +0200

    user matchung page item

---
+++ MatchMateCore/Dtos/UsersViewModels/UserMatchList.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MatchMateCore.Dtos.UsersViewModels
+{
+    public class UserMatchList
+    {
+        public int CurrentPageNumber { get; set; }
+
+        public int NextPageNumber { get; set; }
+        public int PrevoiusPageNumber { get; set; }
+
+        public IList<UserCardModel> Users { get; set; } = new List<UserCardModel>();
+    }
+}
