b67c8d5b8cca37284d8932ffb69dcb6df1e41f08
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-10 23:56:33 +0200

    .

--- MatchMateCore/Interfaces/MongoInterfaces/IProfilePictureInterface.cs
+++ MatchMateCore/Interfaces/MongoInterfaces/IProfilePictureInterface.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace MatchMateCore.Interfaces.MongoInterfaces
 {
     public interface IProfilePictureInterface
     {
+        public Task ChangeProfilePictureMongoAsync(string userId, string file);
         public Task SaveProfilePictureToMongoAsync(string userId,string file);
         public Task<string> GetProfilePictureFromMongoAsync(string userId);
     }
