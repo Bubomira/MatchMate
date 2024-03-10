967a890294ee8dceae053945947c28a1e52f2742
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-10 22:55:38 +0200

    user profile service and view

--- MatchMateCore/Interfaces/EntityInterfaces/UserInterfaces/IUserInterface.cs
+++ MatchMateCore/Interfaces/EntityInterfaces/UserInterfaces/IUserInterface.cs
@@ -1,11 +1,11 @@
 ﻿
-
 using MatchMateCore.Dtos.UsersViewModels;
 
 namespace MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces
 {
     public interface IUserInterface
     {
+        public Task<UserProfileModel> GetCurrentUserInfo(string userId);
         public Task<List<UserCardModel>> GetUsersWithTheSameInterests(string userId,int pageCount);
         public Task AddUserBio(string userBio,string userId);
     }
