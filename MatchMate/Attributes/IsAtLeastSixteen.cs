4e3e45c45f0a02fd25e4ef14c55c648f699b1b05
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-11 11:59:19 +0200

    user has to be at least 16 to use the app attribute

---
+++ MatchMate/Attributes/IsAtLeastSixteen.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchMate.Attributes
+{
+    public class IsAtLeastSixteen : ValidationAttribute
+    {
+        private readonly DateTime date;
+        public IsAtLeastSixteen()
+        {
+            date = DateTime.Now;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+
+                if (((DateTime)value).AddYears(16) < date)
+                {
+                    return ValidationResult.Success;
+                }
+
+            }
+            return new ValidationResult("You have to be at least 16 to use this app!");
+        }
+    }
+}
