2263a83afe9d10bcb9a90a76056dc97daeb34ee3
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-12 23:33:19 +0200

    pending and index views without pagination and controller

--- MatchMate/Controllers/UserControllers/FriendshipController.cs
+++ MatchMate/Controllers/UserControllers/FriendshipController.cs
@@ -18,14 +18,20 @@ namespace MatchMate.Controllers.UserControllers
         [HttpGet]
         public async Task<IActionResult> Index(int pageNumber)
         {
-            var friends = await _friendshipService.GetAllFriendsAsync(User.Id(), pageNumber - 1);
-            return View();
+            var friends = await _friendshipService.GetActiveFriendsAsync(User.Id(), pageNumber - 1);
+
+            foreach (var friend in friends)
+            {
+                friend.ImageUrl = await _profilePictureService.GetProfilePictureFromMongoAsync(friend.UserId);
+            }
+
+            return View(friends);
         }
 
         [HttpGet]
         public async Task<IActionResult> Pending(int pageNumber)
         {
-            var pendingRequests = await _friendshipService.ViewAllPendingRequestAsync(User.Id());
+            var pendingRequests = await _friendshipService.ViewPendingRequestsAsync(User.Id(), pageNumber - 1);
             foreach (var pending in pendingRequests)
             {
                 pending.ImageUrl = await _profilePictureService.GetProfilePictureFromMongoAsync(pending.UserId);
@@ -36,13 +42,14 @@ namespace MatchMate.Controllers.UserControllers
 
         public async Task<IActionResult> SendFriendShipRequest(string id)
         {
-            if(await _friendshipService.CheckIfThereIsARelationShipBetweenUsers(User.Id(), id)){
+            if (await _friendshipService.CheckIfThereIsARelationShipBetweenUsersAsync(User.Id(), id))
+            {
                 return RedirectToAction(nameof(Index), new { pageNumber = 1 });
             }
 
             await _friendshipService.SendFriendRequestAsync(User.Id(), id);
 
-            return RedirectToAction(nameof(Pending),new{pageNumber=1 });
+            return RedirectToAction(nameof(Pending), new { pageNumber = 1 });
         }
 
     }
