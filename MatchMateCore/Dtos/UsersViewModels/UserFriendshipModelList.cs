274faf6999075d10f681712d356f966d3e236010
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-14 22:31:57 +0200

    new user driendship model

---
+++ MatchMateCore/Dtos/UsersViewModels/UserFriendshipModelList.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace MatchMateCore.Dtos.UsersViewModels
+{
+    public class UserFriendshipModelList
+    {
+        public IList<UserCardModel> Friends { get; set; } = new List<UserCardModel>();
+
+        public int TotalFriends { get; set; }
+
+        public int CurrentPage { get; set; } = 1;
+
+        public int PrevousPage { get; set; }
+
+        public int NextPage { get; set; }
+
+        public double TotalPagesCount { get; set; }=0;
+    }
+}
