97fe7e6a23b84cdd3ab27d75820f81aec63d2aef
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-18 23:42:49 +0200

    create offer

---
+++ MatchMate/Controllers/UserControllers/OfferController.cs
@@ -0,0 +1,44 @@
+﻿using MatchMateCore.Dtos.OfferViewModels;
+using MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MatchMate.Controllers.UserControllers
+{
+    public class OfferController : BaseController
+    {
+        private readonly IOfferInterface _offerInterface;
+        public OfferController(IOfferInterface offerInterface)
+        {;
+            _offerInterface = offerInterface;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(string id)
+        {
+            OfferPostFormModel model = new OfferPostFormModel();
+            model.ReceiverUsername = await _offerInterface.GetOfferReceiverUsername(id);
+            model.ReceiverId = id;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Create(OfferPostFormModel offerPostFormModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Create));
+            }
+
+            await _offerInterface.AddOffer(offerPostFormModel, User.Id());
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
