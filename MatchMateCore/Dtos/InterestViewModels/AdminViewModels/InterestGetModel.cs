d8ca1128a4c7458ab80e3ebeba3021ee592829b3
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-24 23:50:38 +0200

    interest panel index view, interest admin card and links

--- MatchMateCore/Dtos/InterestViewModels/AdminViewModels/InterestGetModel.cs
+++ MatchMateCore/Dtos/InterestViewModels/AdminViewModels/InterestGetModel.cs
@@ -2,6 +2,7 @@
 {
     public class InterestGetModel
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int PeopleCount { get; set; }
     }
