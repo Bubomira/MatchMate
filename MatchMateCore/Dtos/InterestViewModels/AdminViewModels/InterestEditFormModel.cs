47cc77777d17888c66e1a3cc2450da09379b0f34
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-25 11:26:22 +0200

    interest edit model improved

--- MatchMateCore/Dtos/InterestViewModels/AdminViewModels/InterestEditFormModel.cs
+++ MatchMateCore/Dtos/InterestViewModels/AdminViewModels/InterestEditFormModel.cs
@@ -4,7 +4,21 @@ namespace MatchMateCore.Dtos.InterestViewModels.AdminViewModels
 {
     public class InterestEditFormModel : InterestPostFormModel
     {
+        public InterestEditFormModel()
+        {
+
+        }
+        public InterestEditFormModel(string name,int id, int currentPage)
+        {
+            Id = id;
+            CurrentPage = currentPage;
+            Name = name;
+        }
+
         [Required]
         public int Id { get; set; }
+
+        //for navigation purposes
+        public int CurrentPage { get; set; }
     }
 }
