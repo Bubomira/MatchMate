a9c37cfde65c5d3a90bb54de9711e22dddd9bcb7
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-26 22:12:01 +0200

    blocked user view models

---
+++ MatchMateCore/Dtos/BlockedUserModels/BlockedUserCard.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchMateCore.Dtos.BlockedUserModels
+{
+    public class BlockedUserCard
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Bio { get; set; } = string.Empty;
+
+
+    }
+}
---
+++ MatchMateCore/Dtos/BlockedUserModels/BlockedUserList.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace MatchMateCore.Dtos.BlockedUserModels
+{
+    public class BlockedUserList
+    {
+        public const int BlockedUsersOnPage = 10;
+        public IList<BlockedUserCard> BlockedUsers { get; set; } = new List<BlockedUserCard>();
+        public int TotalBlockedUsersCount { get; set; }
+        public int MaxItemsOnPage { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
