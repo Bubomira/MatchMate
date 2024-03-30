b9e6f58537b7a285506c60b0d442b91a0f7ec81a
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-30 02:11:31 +0200

    fixed bugs

--- MatchMate/Controllers/BaseControllers/BaseAdminController.cs
+++ MatchMate/Controllers/BaseControllers/BaseAdminController.cs
@@ -4,9 +4,6 @@ namespace MatchMate.Controllers.BaseControllers
 {
     public class BaseAdminController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+
     }
 }
