336668f861ae5242375594dc2ae7861b1cccc0c9
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-30 02:13:07 +0200

    get all reported offers

---
+++ MatchMate/Controllers/AdminControllers/ReportedOffersController.cs
@@ -0,0 +1,25 @@
+﻿using MatchMate.Controllers.BaseControllers;
+using MatchMateCore.Dtos.OfferViewModels.OfferAdminViewModels;
+using MatchMateCore.Interfaces.EntityInterfaces.AdminInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchMate.Controllers.AdminControllers
+{
+    public class ReportedOffersController : BaseAdminController
+    {
+        private readonly IAdminReportOfferInterface _adminReportService;
+
+        public ReportedOffersController(IAdminReportOfferInterface adminReportService)
+        {
+            _adminReportService = adminReportService;
+        }
+
+        public async Task<IActionResult> Index([FromQuery] ReportedOfferListModel reportOfferListModel)
+        {
+            reportOfferListModel.ReportedOffers = await _adminReportService.GetAllReportedOffers(reportOfferListModel);
+            reportOfferListModel.TotalPageCount = Math.Ceiling((double)reportOfferListModel.AllOffersCount/ ReportedOfferListModel.MaxItemsOnPage);
+
+            return View(reportOfferListModel);
+        }
+    }
+}
