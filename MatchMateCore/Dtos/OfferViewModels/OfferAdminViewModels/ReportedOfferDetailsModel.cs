551d0769dd2a77d44bb696f7ca4b226469a71a9b
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-03-31 22:17:30 +0300

    admin reported offer details dto

--- MatchMateCore/Dtos/OfferViewModels/OfferAdminViewModels/ReportedOfferDetailsModel.cs
+++ MatchMateCore/Dtos/OfferViewModels/OfferAdminViewModels/ReportedOfferDetailsModel.cs
@@ -2,7 +2,14 @@
 
 namespace MatchMateCore.Dtos.OfferViewModels.OfferAdminViewModels
 {
-    public class ReportedOfferDetailsModel
+    public class ReportedOfferDetailsModel:ReportedOfferModel
     {
+        public int ReportNumber { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public string Time { get; set; } = string.Empty;
+        public string SuggesterId { get; set; } = string.Empty;
+        public string? Comment { get; set; } = string.Empty;
+        public bool IsValidated { get; set; }
     }
 }
