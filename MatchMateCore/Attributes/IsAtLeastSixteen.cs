15b6d8f9bb4fbbfab99b252e7ea086a410f6fc6c
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-04-01 23:00:36 +0300

    moved attributes place

--- MatchMateCore/Attributes/IsAtLeastSixteen.cs
+++ MatchMateCore/Attributes/IsAtLeastSixteen.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using static MatchMateInfrastructure.DataConstants;
 
-namespace MatchMate.Attributes
+namespace MatchMateCore.Attributes
 {
     public class IsAtLeastSixteen : ValidationAttribute
     {
