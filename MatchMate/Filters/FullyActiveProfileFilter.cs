f3d58b80d5527c0471dcc817a7feb849381dfa23
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-04-05 10:05:55 +0300

    filters for ensuring that users have completed all profile set up after registration steps

---
+++ MatchMate/Filters/FullyActiveProfileFilter.cs
@@ -0,0 +1,40 @@
+﻿using MatchMate.Areas.Matcher.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+using static MatchMateInfrastructure.DataConstants.CustomClaimsType;
+
+namespace MatchMate.Filters
+{
+    public class FullyActiveProfileFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            var user = context.HttpContext.User;
+            var controller = (BaseUserController)context.Controller;
+
+            if(user == null || controller==null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+            }
+
+            if (user.FindFirstValue(HasBio)==null || !bool.Parse(user.FindFirstValue(HasBio)))
+            {
+                context.Result = controller.RedirectToAction("SetUpBio","User");
+            }
+
+            if (user.FindFirstValue(HasInterests) == null || !bool.Parse(user.FindFirstValue(HasInterests)))
+            {
+                context.Result = controller.RedirectToAction("SetUpInterests", "User");
+            }
+
+            if (user.FindFirstValue(HasPfp) == null || !bool.Parse(user.FindFirstValue(HasPfp)))
+            {
+                context.Result = controller.RedirectToAction("SetUpProfilePicture", "User");
+            }
+        }
+    }
+}
