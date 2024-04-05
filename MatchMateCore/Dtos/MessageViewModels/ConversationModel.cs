db70b6fdc98b81f13980efa8d5ba0ab6a3d3b264
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-04-05 22:00:58 +0300

    messages dtos

---
+++ MatchMateCore/Dtos/MessageViewModels/ConversationModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace MatchMateCore.Dtos.MessageViewModels
+{
+    public class ConversationModel
+    {
+        public const int messagesOnScroll = 10;
+        public string ReceiverUsername { get; set; } = string.Empty;    
+        public string ReceiverId { get; set; } = string.Empty;
+        public string SenderId { get; set; } = string.Empty;
+        public IList<MessageModel> Messages { get; set; } = new List<MessageModel>();
+    }
+}
---
+++ MatchMateCore/Dtos/MessageViewModels/MessagePostFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static MatchMateInfrastructure.DataConstants.MessageConstants;
+using static MatchMateInfrastructure.ErrorMessages;
+
+namespace MatchMateCore.Dtos.MessageViewModels
+{
+    public class MessagePostFormModel
+    {
+        public string ReceiverId { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(MaxContentLength, MinimumLength = MinContentLength,
+            ErrorMessage = StringLengthErrorMessage)]
+        public string Content { get; set; } = string.Empty;
+    }
+}
