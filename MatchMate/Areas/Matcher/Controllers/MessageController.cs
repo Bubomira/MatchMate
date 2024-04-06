d8ed105a8a3644fb890ac6a9a4c54144f17b4304
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-04-06 23:18:11 +0300

    load conversation action

---
+++ MatchMate/Areas/Matcher/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using MatchMateCore.Dtos.MessageViewModels;
+using MatchMateCore.Interfaces.EntityInterfaces.UserInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Security.Claims;
+
+namespace MatchMate.Areas.Matcher.Controllers
+{
+    public class MessageController : BaseUserController
+    {
+        private readonly IMessageInterface _messageService;
+        private readonly IFriendshipInterface _friendshipService;
+
+        public MessageController(IMessageInterface messageInterface,
+            IFriendshipInterface friendshipService)
+        {
+            _messageService = messageInterface;
+            _friendshipService = friendshipService;
+
+        }
+        public async Task<IActionResult> LoadConversation(string id)
+        {
+            if (!await _friendshipService.CheckIfThereIsAnActiveFriendshipBetweenUsersAsync(id, User.Id()))
+            {
+                return RedirectToAction("Index", "Friendship");
+            }
+            ConversationModel conversation = new ConversationModel()
+            {
+                ReceiverId = id,
+                SenderId = User.Id()
+            };
+
+            await _messageService.GetConversation(conversation);
+
+            return View(conversation);
+
+        }
+    }
+}
