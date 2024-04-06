d5c99429db3af827d4652a6e46fcc558151e7d81
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-04-06 22:51:20 +0300

    added new fields to convo model

--- MatchMateCore/Dtos/MessageViewModels/ConversationModel.cs
+++ MatchMateCore/Dtos/MessageViewModels/ConversationModel.cs
@@ -4,10 +4,13 @@ namespace MatchMateCore.Dtos.MessageViewModels
 {
     public class ConversationModel
     {
-        public const int messagesOnScroll = 10;
-        public string ReceiverUsername { get; set; } = string.Empty;    
+        public const int messagesOnScroll = 10; 
+        public const int messagesOnInit = 20; 
         public string ReceiverId { get; set; } = string.Empty;
+        public string ReceiverUsername { get; set; } = string.Empty;
         public string SenderId { get; set; } = string.Empty;
         public IList<MessageModel> Messages { get; set; } = new List<MessageModel>();
+        public int MessagesTotal { get; set; }
+        public int ScrollerCount { get; set; }
     }
 }
