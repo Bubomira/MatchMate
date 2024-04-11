69179d5667c8a3ffcea0d7b254c58eee16d5f4ef
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-04-11 23:59:21 +0300

    toastr function

--- MatchMate/wwwroot/js/site.js
+++ MatchMate/wwwroot/js/site.js
@@ -1,1 +1,41 @@
-﻿
+﻿let notify = (message) => {
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    let info = () => {
+        toastr["info"](message)
+    }
+
+    let success = () => {
+        toastr["success"](message)
+    }
+
+    let warning = () => {
+        toastr["warning"](message)
+    }
+
+    let error = () => {
+        toastr["error"](message)
+    }
+
+    return {
+        info, success, warning, error
+    }
+
+
+}
