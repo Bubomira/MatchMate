5aabee75c3cad4e01f74710763c13387c14b66af
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-04-15 10:36:45 +0300

    added set up interest entities

---
+++ MatchMateTests/SetUpEntities/SetUpInterests/SetUpInterests.cs
@@ -0,0 +1,33 @@
+﻿using MatchMateInfrastructure.Models;
+
+using static MatchMateTests.SetUpEntities.SetUpInterests.SetUpUserInterests;
+
+namespace MatchMateTests.SetUpEntities.SetUpInterests
+{
+    public static class SetUpInterests
+    {
+        public static Interest FirstInterest =
+            new Interest()
+            {
+                Id = 1,
+                Name = "Anime",
+               
+            };
+
+        public static Interest SecondInterest =
+            new Interest()
+            {
+                Id = 2,
+                Name = "Painting",
+               
+            };
+
+        public static Interest ThirdInterest =
+            new Interest()
+            {
+                Id = 3,
+                Name = "Hiking",
+              
+            };
+    }
+}
---
+++ MatchMateTests/SetUpEntities/SetUpInterests/SetUpUserInterests.cs
@@ -0,0 +1,55 @@
+﻿using MatchMateInfrastructure.Models;
+
+using static MatchMateTests.SetUpEntities.SetUpInterests.SetUpInterests;
+using static MatchMateTests.SetUpEntities.SetUpUsers;
+
+namespace MatchMateTests.SetUpEntities.SetUpInterests
+{
+    public static class SetUpUserInterests
+    {
+        public static UserInterest FirstUserInterest =
+               new UserInterest()
+               {
+                   UserId = FirstUser.Id,
+                   User = FirstUser,
+                   InterestId = FirstInterest.Id,
+                   Interest = FirstInterest
+               };
+
+        public static UserInterest SecondUserInterest =
+                new UserInterest()
+                {
+                    UserId = SecondUser.Id,
+                    User = SecondUser,
+                    InterestId = FirstInterest.Id,
+                    Interest = FirstInterest
+                };
+
+        public static UserInterest ThirdUserInterest =
+               new UserInterest()
+               {
+                   UserId = SecondUser.Id,
+                   User = SecondUser,
+                   InterestId = SecondInterest.Id,
+                   Interest = SecondInterest
+               };
+
+        public static UserInterest FourthUserInterest =
+               new UserInterest()
+               {
+                   UserId = ThirdUser.Id,
+                   User = ThirdUser,
+                   InterestId = SecondInterest.Id,
+                   Interest = SecondInterest
+               };
+
+        public static UserInterest FifthUserInterest =
+               new UserInterest()
+               {
+                   UserId = ThirdUser.Id,
+                   User = ThirdUser,
+                   InterestId = ThirdInterest.Id,
+                   Interest = ThirdInterest
+               };
+    }
+}
