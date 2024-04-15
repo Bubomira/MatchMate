a77504743bde62540fce521eddb7f3eba5bfa6e8
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-04-15 10:36:53 +0300

    .

--- MatchMateTests/SetUpEntities/SetUpUsers.cs
+++ MatchMateTests/SetUpEntities/SetUpUsers.cs
@@ -1,16 +1,30 @@
 ﻿using MatchMateInfrastructure.Models;
 
+using static MatchMateTests.SetUpEntities.SetUpInterests.SetUpUserInterests;
+
 namespace MatchMateTests.SetUpEntities
 {
     public static class SetUpUsers
     {
-        public static ApplicationUser FirstUser = 
-            new ApplicationUser() { Id = "1", UserName = "Mikael" };
+        public static ApplicationUser FirstUser =
+            new ApplicationUser()
+            {
+                Id = "1",
+                UserName = "Mikael",
+            };
 
-        public static ApplicationUser SecondUser = 
-            new ApplicationUser() { Id = "2", UserName = "Sanya" };
+        public static ApplicationUser SecondUser =
+            new ApplicationUser()
+            {
+                Id = "2",
+                UserName = "Sanya",
+            };
 
-        public static ApplicationUser ThirdUser = 
-            new ApplicationUser() { Id = "3", UserName = "Carl" };
+        public static ApplicationUser ThirdUser =
+            new ApplicationUser()
+            {
+                Id = "3",
+                UserName = "Carl",
+            };
     }
 }
